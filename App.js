560c5340f438b3c9460290812132e59a09d47ee6
kashif2666 <kashifjsr7@gmail.com>
2024-06-02 22:00:50 +0530

    Connected fronted to backend and also shows conversation list

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 //connect to db
 require("./db/connection");
@@ -14,6 +15,7 @@ const Messages = require("./models/Messages");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 const port = process.env.PORT || 8000;
 
 //routes
@@ -60,7 +62,7 @@ app.post("/api/login", async (req, res, next) => {
           res.status(400).send("User email or password is incorrect");
         } else {
           const payload = {
-            userId: user.id,
+            userId: user._id,
             email: user.email,
           };
           const JWT_SECRET_KEY =
@@ -77,13 +79,16 @@ app.post("/api/login", async (req, res, next) => {
                 }
               );
               user.save();
-              next();
+              return res.status(200).json({
+                user: {
+                  id: user._id,
+                  email: user.email,
+                  fullName: user.fullName,
+                },
+                token: token,
+              });
             }
           );
-          res.status(200).json({
-            user: { email: user.email, fullName: user.fullName },
-            token: user.token,
-          });
         }
       }
     }
@@ -105,7 +110,7 @@ app.post("/api/conversation", async (req, res) => {
   }
 });
 
-app.get("/api/conversation/:userId", async (req, res) => {
+app.get("/api/conversations/:userId", async (req, res) => {
   try {
     const userId = req.params.userId;
     const conversations = await Conversation.find({
